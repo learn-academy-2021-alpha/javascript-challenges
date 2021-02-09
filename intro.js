8fad943e3bbe9b63598e44312ac7d3aeae00c9b0
stephbegle <stephbegle@gmail.com>
2021-02-09 15:57:34 -0800

    this is our second commit

--- intro.js
+++ intro.js
@@ -15,22 +15,42 @@ console.log(33 % 6)
 // String Challenges
 console.log()
 
+
 // Find the length of a string containing your name
+console.log("Elyse".length)
 // Does your string include the letter "e"?
+console.log("Elyse".includes("e"))
+
 // Boolean Challenges (return true or false)
 
 // Is 34 divided by 3 greater than 67 divided by 2?
+console.log(34 / 3 > 67 / 2)
 // Does 5 evaluate to the same as "5"?
+console.log(5 == "5")
 // Does 5 strictly equal "5"?
+console.log(5 === "5")
 // Does !3 strictly equal 3?
+console.log(!3 === 3)
 // Does "LEARN".length strictly equal 5 AND "Student".length strictly equal 7?
+console.log("LEARN".length === 5 && "Student".length === 7)
 // Does "LEARN".length strictly equal 5 OR "Student".length strictly equal 10?
+
 // Variables Challenges
 
 // Set a variable called favNum equal to your favorite number
+var favNum = 7
+console.log(favNum)
 // Log your favorite number divided by 2
+console.log(favNum / 2)
 // Set another variable called otherNum equal to 13
+const otherNum = 13
+console.log(otherNum)
 // What is favNum divided by otherNum?
+console.log(favNum / otherNum)
 // Save a string as a variable
+let ourString = "This is our string!"
+console.log(ourString)
 // Find the length of the string
+console.log(ourString.length)
 // Use the method charAt() to find what letter is at the 3rd index
+console.log(ourString.charAt(3))
