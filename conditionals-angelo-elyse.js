7b1ae499dd2d99da198bf12ba642b79366462847
elysemontano <elysemontano@gmail.com>
2021-02-10 11:25:06 -0800

    our code

--- conditionals-angelo-elyse.js
+++ conditionals-angelo-elyse.js
@@ -33,7 +33,7 @@ if(trafficLight === "green"){
 // Write an if/else statement that takes two variables of numbers and outputs the larger number, or logs equal if the numbers are the same.
 //
 var numOne = 4
-var numTwo = 
+var numTwo = 5
 // Write an if/else statement that takes a variable of a number and logs whether the number is odd, even, or zero.
 //
 // STRETCH Challenges
