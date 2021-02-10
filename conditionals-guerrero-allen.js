fd7b19d2038d319e72e3f159125044ccb048c938
algtan <agtan@ymail.com>
2021-02-10 11:27:59 -0800

    2nd Challenge Complete

--- conditionals-guerrero-allen.js
+++ conditionals-guerrero-allen.js
@@ -1,9 +1,23 @@
 // conditional challenges
-var item = 10
+// var item = 10
 
-if (item > 100){
-    console.log("over budget")
+// if (item > 100){
+//     console.log("over budget")
+// } else if(item < 100){
+//     console.log("in budget")
+// }
+
+// Write an if/else statement that takes a variable of hungry and logs "eat food" if you are hungry and "keep coding" if you are not hungry.
+var hungry = false;
+
+if (hungry === true) {
+    console.log("eat food")
+} else if (hungry === false) {
+    console.log("keep coding")
 }
-else if(item < 100){
-    console.log("in budget")
-}
+
+// Write an if/else statement that takes a variable of trafficLight and logs "go" if the light is green, "slow down" if the light is yellow and "stop" if the light is red.
+
+// Write an if/else statement that takes two variables of numbers and outputs the larger number, or logs equal if the numbers are the same.
+
+// Write an if/else statement that takes a variable of a number and logs whether the number is odd, even, or zero.
