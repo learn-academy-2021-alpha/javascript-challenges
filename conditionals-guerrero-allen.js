d8e097810b353e2b29777b11931d8b86e96c275b
algtan <agtan@ymail.com>
2021-02-10 11:57:22 -0800

    Conditionals Challenges Complete

--- conditionals-guerrero-allen.js
+++ conditionals-guerrero-allen.js
@@ -36,6 +36,28 @@
 // } else if (trafficLight === "red"){
 //     console.log("stop!!")
 // } 
+
 // Write an if/else statement that takes two variables of numbers and outputs the larger number, or logs equal if the numbers are the same.
+// var num1 = 100
+// var num2 = 100
+
+// if (num1 < num2) {
+//     console.log(num2)
+// } else if (num1 > num2) {
+//     console.log(`${num1}`);
+// } else if (num1 === num2) {
+//     console.log('numbers are the same')
+// }
 
 // Write an if/else statement that takes a variable of a number and logs whether the number is odd, even, or zero.
+var number = 6
+
+var remainder = number % 2
+
+if (number === 0) {
+    console.log('zero');
+} else if (remainder === 1) {
+    console.log('odd');
+} else if (remainder === 0) {
+    console.log('even');
+}
