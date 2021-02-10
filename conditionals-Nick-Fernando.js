2ba847af4dc017c4ba653b747fb7e100269867d3
romerofernando <romero_f22@yahoo.com>
2021-02-10 11:17:47 -0800

    updated conditionals 1 and 2

---
+++ conditionals-Nick-Fernando.js
@@ -0,0 +1,33 @@
+// Write an if/else statement that takes a variable of item and logs "in budget" if a price is $100 or less.
+
+// Write an if/else statement that takes a variable of hungry and logs "eat food" if you are hungry and "keep coding" if you are not hungry.
+
+// Write an if/else statement that takes a variable of trafficLight and logs "go" if the light is green, "slow down" if the light is yellow and "stop" if the light is red.
+
+// Write an if/else statement that takes two variables of numbers and outputs the larger number, or logs equal if the numbers are the same.
+
+// Write an if/else statement that takes a variable of a number and logs whether the number is odd, even, or zero.
+
+
+
+
+
+
+// var item = 105
+
+// if (item <= 100){
+//     console.log ("you have enough")
+// } else {
+// console.log ("too expensive")
+// }    
+
+var hungry = "no"
+
+if( hungry === "yes"){
+    console.log("eat food")
+}
+else{
+    console.log("keep coding")
+}
+
+
