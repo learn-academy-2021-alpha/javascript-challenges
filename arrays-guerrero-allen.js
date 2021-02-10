cb0efe42ae057830c9b0e253a3ce811a05c403ed
guerrerocampos <jrheezy.gc@gmail.com>
2021-02-10 14:49:31 -0800

    challenge 2 completed

--- arrays-guerrero-allen.js
+++ arrays-guerrero-allen.js
@@ -1,12 +1,26 @@
 // 1. Consider this variable:
 //   var groceryList = ["chips", "dip", "cookies"]
-var groceryList = ["chips", "dip", "cookies"]
+// var groceryList = ["chips", "dip", "cookies"]
 // Write the code that will add "soda" to the end of the original array.
-groceryList.push("soda")
-console.log(groceryList)
+// groceryList.push("soda")
+// console.log(groceryList)
 // Write the code that will add "granola" to the end of array without altering the original array.
-var granola = "granola"
-var fullList = groceryList.concat(granola)
-console.log(fullList)
+// var granola = "granola"
+// var fullList = groceryList.concat(granola)
+// console.log(fullList)
 
-// 2.
+// var numbers = [2, 4, 6, 8, 10]
+
+// Write the code that will add the number 0 to the beginning of the array.
+// numbers.unshift(0)
+// console.log(numbers)
+// Write the code that will add the number 12 to the end of the array.
+// numbers.push(12)
+// console.log(numbers)
+// Write the code that will remove the first number from the array.
+// numbers.shift()
+// console.log(numbers)
+// Write the code that will add the number 0 to the beginning of the array without altering the original array. HINT: it's not .unshift You'll have to get creative! ;
+// var numbers2 = [0]
+// var allNumbers = numbers2.concat(numbers)
+// console.log(allNumbers)
