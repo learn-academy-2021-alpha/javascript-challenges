da252a41bcc03fa7c8428d6a22dd04399ee2445d
DevenPerkins <deven.s.perkins@gmail.com>
2021-02-10 15:15:45 -0800

    a few more done

--- arrays-lex-deven.js
+++ arrays-lex-deven.js
@@ -1,23 +1,28 @@
 // Consider this variable:
-let groceryList = ["chips", "dip", "cookies"]
-// Write the code that will add "soda" to the end of the original array.
-groceryList.push("soda");
-console.log(groceryList);
+// let groceryList = ["chips", "dip", "cookies"]
+// // Write the code that will add "soda" to the end of the original array.
+// groceryList.push("soda");
+// console.log(groceryList);
 // Write the code that will add "granola" to the end of array without altering the original array.
-var granola = ["granola"]
-let groceryList2 = groceryList.concat(granola);
-console.log(groceryList);
-console.log(groceryList2);
+// var granola = ["granola"]
+// let groceryList2 = groceryList.concat(granola);
+// console.log(groceryList);
+// console.log(groceryList2);
 // Consider this variable:
-//   var numbers = [2, 4, 6, 8, 10]
+var numbers = [2, 4, 6, 8, 10]
 // Write the code that will add the number 0 to the beginning of the array.
-//
+numbers.unshift(0)
+// console.log(numbers)
 // Write the code that will add the number 12 to the end of the array.
-//
+numbers.push(12)
+// console.log(numbers)
 // Write the code that will remove the first number from the array.
-//
+numbers.shift()
+console.log(numbers)
 // Write the code that will add the number 0 to the beginning of the array without altering the original array. HINT: it's not .unshift You'll have to get creative! ;)
-//
+var numbers3 = [0]
+var numbers2 = numbers3.concat(numbers)
+console.log(numbers2)
 // Consider this variable:
 //   var numSet = [2, 13, 6, 8, 4, 2]
 // Write the code that finds the index of the first appearance of the number 2.
