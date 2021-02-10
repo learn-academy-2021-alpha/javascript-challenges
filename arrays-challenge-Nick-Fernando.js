c20dad6b99cc936340a2078354e18b232ca722dc
romerofernando <romero_f22@yahoo.com>
2021-02-10 14:56:04 -0800

    updated arrays challanges

--- arrays-challenge-Nick-Fernando.js
+++ arrays-challenge-Nick-Fernando.js
@@ -1,6 +1,6 @@
 // Consider this variable:
 
-var groceryList = ["chips", "dip", "cookies"]
+// var groceryList = ["chips", "dip", "cookies"]
 
 // Write the code that will add "soda" to the end of the original array.
 
@@ -9,27 +9,45 @@ var groceryList = ["chips", "dip", "cookies"]
 
 // Write the code that will add "granola" to the end of array without altering the original array.
 
-groceryList.push("granola");
-console.log(groceryList);
+// groceryList.push("granola");
+// console.log(groceryList);
 
 // Consider this variable:
 //   var numbers = [2, 4, 6, 8, 10]
 // Write the code that will add the number 0 to the beginning of the array.
 
+
+// numbers.unshift(0)
+// console.log(numbers)
+
 // Write the code that will add the number 12 to the end of the array.
 
+
+// numbers.push(12)
+// console.log(numbers)
 // Write the code that will remove the first number from the array.
 
-// Write the code that will add the number 0 to the beginning of the array without altering the original array. HINT: it's not .unshift You'll have to get creative! ;)
+// numbers.shift()
+// console.log(numbers)
+
+// Write the code that will add the number 0 to the beginning of the array without altering the original array. HINT: it's not .unshift You'll() have to get creative! ;)
+
+// numbers.splice(0, 0, 0)
+// console.log(numbers)
 
 // Consider this variable:
-//   var numSet = [2, 13, 6, 8, 4, 2]
-// Write the code that finds the index of the first appearance of the number 2.
+ var numSet = [2, 13, 6, 8, 4, 2]
+
+ // Write the code that finds the index of the first appearance of the number 2.
+
+console.log(numSet.indexOf(2))
 
 // Write the code that finds the index of the last appearance of the number 2.
 
+console.log(numSet.lastIndexOf(2))
+
 // Consider this variable:
-//  var chars = ["y", "a", "r", "r", "a"]
+//var chars = ["y", "a", "r", "r", "a"]
 // Write the code that brings all the letters in the chars array together into a string.
 
 // Write the code that reverses the order of the letters in the chars array and saves it into a variable called charsReversed.
