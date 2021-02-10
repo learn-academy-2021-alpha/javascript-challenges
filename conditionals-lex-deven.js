c901531182435f98fc22b3f065cdc1d0058f252e
DevenPerkins <deven.s.perkins@gmail.com>
2021-02-10 11:54:05 -0800

    two more challenges in the books

--- conditionals-lex-deven.js
+++ conditionals-lex-deven.js
@@ -2,24 +2,51 @@
 // Make sure you try different options and change the variables to ensure properly working code.
 //
 // Write an if/else statement that takes a variable of item and logs "in budget" if a price is $100 or less.
-var item = 1000
-if(item<=100){
-  console.log("in budget")
-} else{
-  console.log("you're broke")
-}
+// var item = 1000
+// if(item<=100){
+//   console.log("in budget")
+// } else{
+//   console.log("you're broke")
+// }
 //
 // Write an if/else statement that takes a variable of hungry and logs "eat food" if you are hungry and "keep coding" if you are not hungry.
-var hungry = false
-if(hungry===true){
-  console.log("eat food");
-} else{
-  console.log("keep coding");
-}
+// var hungry = false
+// if(hungry===true){
+//   console.log("eat food");
+// } else{
+//   console.log("keep coding");
+// }
 //
 // Write an if/else statement that takes a variable of trafficLight and logs "go" if the light is green, "slow down" if the light is yellow and "stop" if the light is red.
+// var trafficLight = "dark"
+// if(trafficLight === "green"){
+//     console.log("go")
+// }
+// else if(trafficLight === "yellow"){
+//     console.log("slow down")
+// }
+// else if(trafficLight === "red"){
+//     console.log("stop")
+// }
+// else {
+//     console.log("open your eyes")
+// }
 //
 // Write an if/else statement that takes two variables of numbers and outputs the larger number, or logs equal if the numbers are the same.
+// var num1 = 42
+// var num2 = "poop"
+
+// if(num1 > num2){
+//     console.log(`${num1} is the larger number`)
+// }else if(num1 < num2){
+//     console.log(`${num2} is the larger number`)
+// }
+// else if (num1 === num2){
+//     console.log(`${num1} is equal to ${num2}`)
+// }
+// else{
+//     console.log("something went wrong")
+// }
 //
 // Write an if/else statement that takes a variable of a number and logs whether the number is odd, even, or zero.
 //
