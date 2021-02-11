e333c17918585a62a459b584bbae344c01b81a16
algtan <agtan@ymail.com>
2021-02-10 16:57:13 -0800

    challenge 2 complete

--- loops-guerrero-allen.js
+++ loops-guerrero-allen.js
@@ -16,3 +16,41 @@
 //         console.log("ODD")
 //     }
 //     }
+
+// Challenge #2
+var nums = [3, 57, -9, 20, 67]
+// Create a loop that will log the highest number from the array. Expected output --> 67
+
+// Initialize a variable to STORE the highest number
+// var highestNum = nums[0];
+
+// for (let i = 1; i < nums.length; i++) {
+//     // Check if the next element in the array is greater than the current highest number
+//     if (nums[i] > highestNum) {
+//         // The highest number is now index 'i' of array 'nums'
+//         highestNum = nums[i];
+//     }
+// }
+// // Log the highest number after going through the array
+// console.log(highestNum)
+
+
+// Create a loop that will log the lowest number from the array Expected output --> -9
+
+// var lowestNum = nums[0];
+
+// for (let i = 1; i < nums.length; i++) {
+//     // Check if the next element in the array is greater than the current highest number
+//     if (nums[i] < lowestNum) {
+//         // The highest number is now index 'i' of array 'nums'
+//         lowestNum = nums[i];
+//     }
+// }
+// Log the highest number after going through the array
+// console.log(lowestNum)
+
+// Create a loop that will log the remainder of each number when divided by 2. Expected output --> 1, 1, -1, 0, 1
+
+for (let i = 0; i < nums.length; i++) {
+    console.log( nums[i] % 2 );
+}
