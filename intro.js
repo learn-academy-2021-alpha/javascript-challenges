f2f04ef33b618e15646d2d476b87387a41a9011f
sjproctor <sjproctor950@gmail.com>
2021-02-11 06:57:51 -0800

    adds arithmetic challenges

--- intro.js
+++ intro.js
@@ -1,1 +1,19 @@
 // JavaScript Intro Challenges
+
+// Add 34 and 71
+console.log(34 + 71)
+
+// Subtract 67 from 123
+console.log(123 - 67)
+
+// Multiply 56 and 23
+console.log(56 * 23)
+
+// Divide 45 by 5
+console.log(45 / 5)
+
+// Calculate 5 to the power of 7
+console.log(5 ** 7)
+
+// Find the remainder of 33/6
+console.log(33 % 6)
