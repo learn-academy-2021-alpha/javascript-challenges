ff8d614ba781e2ee2fa9c4abf21b262e4397302f
justinhilario <justinhilario@gmail.com>
2021-02-11 11:57:48 -0800

    four

--- functions-justin-jr.js
+++ functions-justin-jr.js
@@ -7,8 +7,24 @@
 
 // Write a function named oddOrEven that takes a number as an argument and returns whether the number is odd or even.
 
+// const oddOrEven = (number) => {
+//     if (number % 2 === 0){
+//         return "Your number is even!"
+//     } else if (number % 2 === 1){
+//         return "Your number is odd!"
+//     }
+// }
+
+// console.log(oddOrEven(5))
+// console.log(oddOrEven(8))
+
 // Write a function named triple that takes a number as an argument and returns the result of that number multiplied by 3.
 
+// const triple = (number) => {
+//     return (number * 3)
+// }
+
+// console.log(triple(2))
 // Write a function named multiply that takes two numbers as arguments and returns the result of one of the numbers multiplied by the other.
 
 // Write a function named divisibleBy that takes two numbers as arguments and returns whether the first number is evenly divisible by the second so that divisibleBy(10, 5) logs "10 is evenly divisible by 5".
