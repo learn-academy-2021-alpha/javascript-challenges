eb4fdb919290ac5c896dd0c8a12bc2e6fe917dd9
nickdsaunders <nickdavidsaunders@gmail.com>
2021-02-11 12:28:18 -0800

    updated functions challenge part 2

---
+++ conditionals-Nick-Fernando.js
@@ -0,0 +1,69 @@
+// Write an if/else statement that takes a variable of item and logs "in budget" if a price is $100 or less.
+
+// Write an if/else statement that takes a variable of hungry and logs "eat food" if you are hungry and "keep coding" if you are not hungry.
+
+// Write an if/else statement that takes a variable of trafficLight and logs "go" if the light is green, "slow down" if the light is yellow and "stop" if the light is red.
+
+// Write an if/else statement that takes two variables of numbers and outputs the larger number, or logs equal if the numbers are the same.
+
+// Write an if/else statement that takes a variable of a number and logs whether the number is odd, even, or zero.
+
+
+
+
+
+
+// var item = 105
+
+// if (item <= 100){
+//     console.log ("you have enough")
+// } else {
+// console.log ("too expensive")
+// }    
+
+// var hungry = "no"
+
+// if( hungry === "yes"){
+//     console.log("eat food")
+// }
+// else{
+//     console.log("keep coding")
+// }
+
+// var trafficLight = "blue"
+
+// if (trafficLight === "green"){
+//     console.log("go")
+// } else if (trafficLight === "yellow"){
+//     console.log("slow down")
+// } else if (trafficLight === "red"){
+//     console.log("stop")
+// } else{
+//     console.log("you crashed")
+// }
+
+// var num1 = 10
+// var num2 = "string"
+
+// if (num1 > num2){
+//     console.log(`${num1} is larger`)
+// } else if (num1 < num2){
+//     console.log(`${num2} is larger`)
+// } else if (num1 === num2){
+//     console.log(`${num1} and ${num2} are the same`)
+// } else {
+//     console.log("try again")
+// }
+
+var number1 = "string"
+
+if(number1 % 2 === 0 && number1 === 0 ){
+    console.log ("this number is zero")
+}
+else if(number1 % 2 === 1){
+    console.log ("this number is odd")
+} else if(number1 % 2 === 0 ){
+    console.log ("this number is even")
+} else {
+    console.log("try using a number")
+}
--- functions-nick-deven.js
+++ functions-nick-deven.js
@@ -1,19 +1,19 @@
 // Write a function named marco that returns "polo".
 // create a function that takes no argument named marco that gives us a return of "polo"
 
-const marco = () =>{
-    return "polo"
-}
-console.log(marco())
+// const marco = () =>{
+//     return "polo"
+// }
+// console.log(marco())
 
-// Write a function named greeting that takes a name as an argument and returns "Welcome, <person's name here>!"
+// // Write a function named greeting that takes a name as an argument and returns "Welcome, <person's name here>!"
 
-// Create a function that takes in an argument and will return the prompt "Welcome , person's name"
+// // Create a function that takes in an argument and will return the prompt "Welcome , person's name"
 
-const greeting = (personsName) =>{
-    return `Welcome, ${personsName}`
-}
-console.log(greeting("Dave"))
+// const greeting = (personsName) =>{
+//     return `Welcome, ${personsName}`
+// }
+// console.log(greeting("Dave"))
 // Write a function named oddOrEven that takes a number as an argument and returns whether the number is odd or even.
 
 // Create a function that takes in a number as an argunent and returns the value odd or even
@@ -21,25 +21,55 @@ console.log(greeting("Dave"))
         // if the number is even return a sentence that says "your number is even"
         // if the number is in neither or not a number "something went wrong"
 
-const oddOrEven = (yourNumber) => {
-    if (yourNumber%2 === 0){
-        return `${yourNumber} is even`
-    }
-    else if (yourNumber%2 === 1){
-        return `${yourNumber} is odd`
-    }
-    else{
-        return "Something went wrong"
-    }
-}
-console.log(oddOrEven(7))
-console.log(oddOrEven(12))
-console.log(oddOrEven("boom"))
+// const oddOrEven = (yourNumber) => {
+//     if (yourNumber%2 === 0){
+//         return `${yourNumber} is even`
+//     }
+//     else if (yourNumber%2 === 1){
+//         return `${yourNumber} is odd`
+//     }
+//     else{
+//         return "Something went wrong"
+//     }
+// }
+// console.log(oddOrEven(7))
+// console.log(oddOrEven(12))
+// console.log(oddOrEven("boom"))
 
 // Write a function named triple that takes a number as an argument and returns the result of that number multiplied by 3.
+// Create a function called triple with an argument and returns the argument multiplied by 3.
+
+// const triple = (myNumber) => {
+//     return (myNumber * 3)
+// }
+// console.log(triple(5))
 
 // Write a function named multiply that takes two numbers as arguments and returns the result of one of the numbers multiplied by the other.
+// Create a function named multiply that will take two arguments: multiple1, multiple2 
+//returns multiple1 multiplied by multiple2
+
+// const multiply = (multiple1 , multiple2) => {
+//     return (multiple1 * multiple2)
+// }
+// console.log(multiply(42 , 2))
+
 
 // Write a function named divisibleBy that takes two numbers as arguments and returns whether the first number is evenly divisible by the second so that divisibleBy(10, 5) logs "10 is evenly divisible by 5".
+// create a function and call it divisibleBy that takes in two arguments: divisible1, divisible2
+// if divisble1 is evenly divisible by the divisible2 return "divisible1 is evenly divisible by divisible2."
+// if divisble1 is not evenly divisible by the divisible2 return "divisible1 is not evenly divisible by divisible2."
+// else return "try again."
+
+const divisibleBy = (divisible1, divisible2) => {
+    if (divisible1 % divisible2 === 0){
+        return `${divisible1} is evenly divisible by ${divisible2}.`
+    } else if (divisible1 % divisible2 === 1){
+        return `${divisible1} is not evenly divisible by ${divisible2}.`
+    } else {
+        return "try agian"
+    }
+}
+console.log(divisibleBy("hey" , 5))
+
 
 // Write a function named assignGrade that takes a number score as an argument and returns the letter grade for the score.
