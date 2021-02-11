dfbd02cec6c8ab6949b663cde183bc02be3dd0da
algtan <agtan@ymail.com>
2021-02-11 11:34:58 -0800

    challenge 1 complete

---
+++ functions-allen-raul.js
@@ -0,0 +1,20 @@
+// Write a function named marco that returns "polo".
+// Create a function named 'marco' that takes no arguments
+const marco = () => {
+    // Return a string "polo"
+    return "polo";
+}
+console.log( marco() );
+
+
+// Write a function named greeting that takes a name as an argument and returns "Welcome, <person's name here>!"
+
+// Write a function named oddOrEven that takes a number as an argument and returns whether the number is odd or even.
+
+// Write a function named triple that takes a number as an argument and returns the result of that number multiplied by 3.
+
+// Write a function named multiply that takes two numbers as arguments and returns the result of one of the numbers multiplied by the other.
+
+// Write a function named divisibleBy that takes two numbers as arguments and returns whether the first number is evenly divisible by the second so that divisibleBy(10, 5) logs "10 is evenly divisible by 5".
+
+// Write a function named assignGrade that takes a number score as an argument and returns the letter grade for the score.
