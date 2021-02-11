e1e7b90e574d7fa7a907d162834905bb36168944
nickdsaunders <nickdavidsaunders@gmail.com>
2021-02-11 09:37:58 -0800

    updated arrays-challenges

--- arrays-challenge-Nick-Fernando.js
+++ arrays-challenge-Nick-Fernando.js
@@ -36,25 +36,36 @@
 // console.log(numbers)
 
 // Consider this variable:
- var numSet = [2, 13, 6, 8, 4, 2]
+ //var numSet = [2, 13, 6, 8, 4, 2]
 
  // Write the code that finds the index of the first appearance of the number 2.
 
-console.log(numSet.indexOf(2))
+//console.log(numSet.indexOf(2))
 
 // Write the code that finds the index of the last appearance of the number 2.
 
-console.log(numSet.lastIndexOf(2))
+//console.log(numSet.lastIndexOf(2))
 
 // Consider this variable:
-//var chars = ["y", "a", "r", "r", "a"]
+var chars = ["y", "a", "r", "r", "a"]
 // Write the code that brings all the letters in the chars array together into a string.
+
+//console.log(chars.join());
 
 // Write the code that reverses the order of the letters in the chars array and saves it into a variable called charsReversed.
 
+
+var charsReversed = (chars.reverse());
+console.log(charsReversed)
+
 // Write the code that brings all the letters in the charsReversed array together into a string with a hyphen between each letter.
+let joinedCharHyphen = charsReversed.join("-");
+console.log(joinedCharHyphen);
 
 // Write the code that brings all the letters in the charsReversed Array together into a string without separators.
+
+let joinedChar = charsReversed.join("");
+console.log(joinedChar);
 
 // Create two arrays consisting of three first names of your cohort members in each.
 // Sort the names in alphabetical order.
