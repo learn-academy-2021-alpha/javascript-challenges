828879ba180e17006c6141b9d07ac4a5c79645ba
LEARN Academy <>
2021-02-11 16:21:27 -0800

    second grabber

--- function-F-A.js
+++ function-F-A.js
@@ -21,22 +21,32 @@
 
 // Write a function named oddOrEven that takes a number as an argument and returns whether the number is odd or even.
 
-// setup function with argument takes an number and determeins if its even or onloadeddata.  
+// setup function with argument takes an number and determeins if its even or onloadeddata.
 
-const evenOrOdd = (number) =>{
-    if (number % 2  === 0)
-    return "Even"
-    else if(number % 2 === 1)
-    return "odd"
-}
-
-console.log(evenOrOdd(3))
+// const evenOrOdd = (number) =>{
+//     if (number % 2  === 0)
+//     return "Even"
+//     else if(number % 2 === 1)
+//     return "odd"
+// }
+//
+// console.log(evenOrOdd(3))
 
 // Write a function named triple that takes a number as an argument and returns the result of that number multiplied by 3.
 
+//setup a function with an argument that takes one number and * 3
+// const triple = (numOne)=>{
+//   return (numOne * 3)
+// }
+// console.log(triple(5));
+
 // Write a function named multiply that takes two numbers as arguments and returns the result of one of the numbers multiplied by the other.
 
+//setup a function that takes two numbers * by eachother and returns results
+// const multiply = (numOne , numTwo) => {
+// return (numOne * numTwo)
+// }
+// console.log(multiply(5 , 5));
 // Write a function named divisibleBy that takes two numbers as arguments and returns whether the first number is evenly divisible by the second so that divisibleBy(10, 5) logs "10 is evenly divisible by 5".
 
 // Write a function named assignGrade that takes a number score as an argument and returns the letter grade for the score.
-
