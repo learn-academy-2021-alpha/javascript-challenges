b290094d0438073f1f0eda5d9f87d31f82cb96d2
LEARN Academy <>
2021-02-12 12:19:32 -0800

    3rd switch back

--- high-order-functions-ang-dev.js
+++ high-order-functions-ang-dev.js
@@ -19,28 +19,42 @@
 
 // [30, 90, 150, 40, 100]
 // Write a function that takes in an array and returns a new array with only odd numbers
-var arr2 = [2, 7, 3, 5, 8, 10, 13]
-// Expected output:[7, 3 ,5 ,13]
+// var arr2 = [2, 7, 3, 5, 8, 10, 13]
+// // Expected output:[7, 3 ,5 ,13]
+//
+// // Set up function that retuns odd number in new array
+// // output should be only the odd numbers
+//
+// const oddNumbers = (array) =>{
+//     return array.filter(value =>{
+//         return value %2 !== 0
+//     })
+// }
+//
+// console.log(oddNumbers(arr2))
 
-// Set up function that retuns odd number in new array
-// output should be only the odd numbers
-
-const oddNumbers = (array) =>{
-    return array.filter(value =>{
-        return value %2 !== 0
-    })
-}
-
-console.log(oddNumbers(arr2))
 // [7, 3, 5, 13]
 // Write a function that takes in an array of numbers and letters and returns a string with only the letters. HINT: Use typeof method
-// var comboArr = [7, "n", "i", "c", 10, "e", false, "w", 3, "o", "r", "k"]
-// Expected output:
+var comboArr = [7, "n", "i", "c", 10, "e", false, "w", 3, "o", "r", "k"]
+// Expected output: "nicework"
+const niceWork = (array) =>{
+  return array.filter(value => {
+    let letters = [].join("")
+    return typeof value === "string"
+    for (let i = 0; i < array.length; i++) {
+      letters.push(array[i])
+    }
+  })
 
+}
+console.log(niceWork(comboArr));
+//setup function that specifies letters and numbers only returning letters
+// function reads array and identifies the letters and gives a string of "nicework"
 // "nicework"
+
 // STRETCH Challenges
 // Create a function that takes in a string and returns a new string with all the vowels removed.
-//  var str = "javascript is awesome" 
+//  var str = "javascript is awesome"
 // Expected output:
 
 // "jvscrpt s wsm"
