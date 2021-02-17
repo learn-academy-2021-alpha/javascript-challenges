2b8131fbed9804dd604926b9dc224d85e8d032f3
guerrerocampos <jrheezy.gc@gmail.com>
2021-02-17 11:45:44 -0800

    challenge one complete

---
+++ objects-g-a.js
@@ -0,0 +1,25 @@
+// var person = {
+//     firstName: "Arthur",
+//     lastName: "Dent",
+//     homePlanet: "earth"
+//   }
+
+//   Write the code that accesses the first name of the person object.
+// console.log(person.firstName)
+
+// // Write the code that accesses the last name of the person object.
+// console.log(person.lastName)
+// Write the code that gives the person object a property of homePlanet and set it to 'Earth'.
+var person = {
+    firstName: "Arthur",
+    lastName: "Dent",
+    homePlanet: "earth",
+    getData: function () {
+        return `${this.firstName} ${this.lastName} is from planet ${this.homePlanet}`
+      }
+    }
+  
+  console.log(person.getData())
+
+  
+// Update the person object with a method that logs "Arthur Dent is from planet Earth".
